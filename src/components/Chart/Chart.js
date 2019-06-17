eb1be88ba8e6bf1a27e65c5fc5f9f2721c905701
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-06-17 15:54:45 +0200

    create svg with dynamic size, x-axis labels with adjusting font-size

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,10 @@
 // @flow
+import { last, rangeStep } from 'lodash/fp';
 import * as React from 'react';
 import Papa from 'papaparse';
 import { withContentRect } from 'react-measure';
 import type { HOC } from 'recompose';
+import { scaleLinear } from 'd3';
 
 // https://www.who.int/childgrowth/standards/tab_wfa_boys_p_0_5.txt
 // $ExpectError
@@ -11,11 +13,11 @@ import whoData from '../../data/tab_wfa_boys_p_0_5.txt';
 import styles from './styles.css';
 
 type Point = {|
-  L: string,
-  M: string,
-  S: string,
+  L: number,
+  M: number,
+  S: number,
 
-  Month: string,
+  Month: number,
 
   P01: number,
   P1: number,
@@ -35,7 +37,10 @@ type Point = {|
 |}
 
 const dataString = atob(whoData.split(',')[1]);
-const formatted: Array<Point> = Papa.parse(
+type FormattedData = {
+  data: Array<Point>,
+};
+const formatted: FormattedData = Papa.parse(
   dataString.slice(0, -1), // remove last new-line character
   {
     header: true, // get keys based on first row
@@ -62,25 +67,95 @@ type EnhancedProps = {|
 
 const enhance: HOC<EnhancedProps, Props> = withContentRect('bounds');
 
+const SVG_HORIZONTAL_PADDING = 20;
+
 const Chart = ({
   contentRect: {
     bounds,
   },
   measureRef,
-}: EnhancedProps) => { // eslint-disable-line arrow-body-style
-  console.debug({ formatted }); // eslint-disable-line
+}: EnhancedProps) => {
+  const { data } = formatted;
   if (!bounds.width) {
     return (<div ref={measureRef} className={styles.container} />);
   }
-  const chartWidth = bounds.width - 100;
-  const chartHeight = chartWidth * 3 / 4;
+  const svgWidth = bounds.width - 100;
+
+  const MAX_SVG_HEIGHT = 620; // prevent a situation when chart needs to be scrolled down
+  const svgHeight = Math.min(svgWidth * 3 / 4, MAX_SVG_HEIGHT);
+
+  const chartWidth = svgWidth - 2 * SVG_HORIZONTAL_PADDING;
+
+  const VIEWBOX_X = 500;
+  const VIEWBOX_Y = VIEWBOX_X * svgHeight / svgWidth;
+  const SVG_PADDING = 40;
+
+  const maxMonth = last(data).Month;
+  const xScale = scaleLinear()
+    .domain([0, maxMonth])
+    .range([SVG_PADDING, VIEWBOX_X - SVG_PADDING])
+    // .clamp(true)
+    .nice();
+
+  const monthRangeWidth = chartWidth / data.length;
+
+  let monthsBetweenTicks;
+  // when there's not enough space for x-axis ticks, don't show some of them
+  // only possible options are 2, 3, 4 and 6 so we always see round year
+  if (monthRangeWidth > 16) {
+    monthsBetweenTicks = 2;
+  } else if (monthRangeWidth > 10) {
+    monthsBetweenTicks = 3;
+  } else if (monthRangeWidth > 6.5) {
+    monthsBetweenTicks = 4;
+  } else {
+    monthsBetweenTicks = 6;
+  }
+
+  const xScaleTicks = rangeStep(
+    monthsBetweenTicks,
+    0,
+    data.length,
+  );
+
+  const isYearTick = tick => tick % 12 === 0;
+  const transformTick = tick => (
+    isYearTick(tick)
+      ? (`${tick / 12} years`)
+      : tick % 12
+  );
+
+  const sizeInPx = pixels => VIEWBOX_X / svgWidth * pixels;
   return (
     <div ref={measureRef} className={styles.container}>
       <svg
         className={styles.svg}
-        width={chartWidth}
-        height={chartHeight}
-      />
+        viewBox={`0 0 ${VIEWBOX_X} ${VIEWBOX_Y}`}
+      >
+        {/* x axis descriptions */}
+        <g>
+          {xScaleTicks.map(tick => (
+            <React.Fragment key={tick}>
+              <text
+                x={xScale(tick)}
+                y={VIEWBOX_Y - (isYearTick(tick) ? 5 : 25)}
+                textAnchor="middle"
+                className={styles.minorXTick}
+                fontSize={sizeInPx(isYearTick(tick) ? 16 : 12)}
+              >
+                {transformTick(tick)}
+              </text>
+              <line
+                x1={xScale(tick)}
+                x2={xScale(tick)}
+                y1={VIEWBOX_Y - 35}
+                y2={VIEWBOX_Y - 43}
+                stroke="black"
+              />
+            </React.Fragment>
+          ))}
+        </g>
+      </svg>
     </div>
   );
 };
